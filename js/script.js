849144be7ed26b31a2c57a065e269b833d000bf6
John Laffey <laffeychem@gmail.com>
2022-11-27 10:37:24 -0500

    remove button and just make dad-img the button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 const jokeSpace = document.getElementById('dad-joke');
-const button = document.getElementById('btn');
+const button = document.getElementById('dad-img');
 
 async function getJoke() {
   const response = await fetch('https://icanhazdadjoke.com', {
